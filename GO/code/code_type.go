0b1585aedc4c3a2351ce373b615ee7b916652cb7
落花浅忆 <740008538@qq.com>
2017-10-23 17:34:54 +0800

    修改文件

--- GO/code/code_type.go
+++ GO/code/code_type.go
@@ -13,10 +13,13 @@ func main() {
 	var a [10]int //数组
 	var b []int   //切片 slice
 	var c 文本
-	d := 999                  //简短声明
-	e, f, g := 1, "hellow", 2 //多变量声明简写（有值）
-	var h, i, j, k int        //函数内多变量声明（无值）
+	d := 999                    //简短声明
+	e, f, g := 1, "hellow", 2.5 //多变量声明简写（有值）
+	var h, i, j, k int          //函数内多变量声明（无值）
 
 	h, _, j, k = 1, 2, 3, 4 //_忽略赋值
+
+	// 类型转行
+	i = int(g)
 	fmt.Print(e, g, f, a, b, c, d, h, i, j, k)
 }
